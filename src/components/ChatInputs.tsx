11e1f3b1f609f77adbb0538e00e7ea50180f08ee
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-06-26 21:49:48 +0530

    Chat Accordian Setup.

---
+++ src/components/ChatInputs.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ChatInputsProps {
+  
+}
+
+const ChatInputs: FC<ChatInputsProps> = ({}) => {
+  return <div>ChatInputs</div>
+}
+
+export default ChatInputs
